d33244db70764a7d3c4d87faffd54102f26c1884
The_Tech_Lead <58194343+wisdomekpotu@users.noreply.github.com>
2022-04-13 14:19:39 +0100

    added new changes

--- main.js
+++ main.js
@@ -6,6 +6,18 @@ const form = document.querySelector("#new-task-form");
 const input = document.querySelector("#new-task-input");
 const list_el = document.querySelector("#tasks");
 
+
+
+//add todo
+form.onsubmit = async (event) => {
+	event.preventDefault();
+	const todo = input.value;
+	await db.todos.add({ todo })
+	await getTodos()
+	form.reset()
+};
+
+//display todo
 const getTodos = async () => {
 	const allTodos = await db.todos.reverse().toArray()
 	list_el.innerHTML = allTodos.map(todo => `
@@ -15,42 +27,16 @@ const getTodos = async () => {
 	<input id="edit" class="text" readonly="readonly" type="text" value= ${todo.todo}>
 	</div>
 	<div class="actions">
-	<button class="edit" onclick="updateTodo(event, ${todo.id})">edit</button>
 	<button class="delete" onclick="deleteTodo(event, ${todo.id})">Delete</button>
 	</div>
 	</div>
-	`).join(" ")
+	`).join("")
 
 }
 window.onload = getTodos
 
+//delete todo
 const deleteTodo = async (event, id) => {
 	await db.todos.delete(id)
 	await getTodos()
-
 }
-
-const updateTodo = async (event, id) => {
-
-	const editbtn = document.querySelector(".edit");
-	const editinput = document.querySelector(".text");
-	if (editbtn.innerText.toLowerCase() == "edit") {
-		editbtn.innerText = "Save";
-		editinput.removeAttribute("readonly");
-		editinput.focus();
-	} else {
-		editbtn.innerText = "Edit";
-		editinput.setAttribute("readonly", "readonly");
-	}
-	await db.todos.update(id)
-
-}
-
-
-form.onsubmit = async (event) => {
-	event.preventDefault();
-	const todo = input.value;
-	await db.todos.add({ todo })
-	await getTodos()
-	form.reset()
-};
